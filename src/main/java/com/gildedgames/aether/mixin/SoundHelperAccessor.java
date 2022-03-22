d9d2bba5b82188ecc854d813cf7be26316491e1f
GameHerobrine <GameHerobrine@users.noreply.github.com>
2022-03-22 12:31:50 +0300

    small sound fixes

--- src/main/java/com/gildedgames/aether/mixin/SoundHelperAccessor.java
+++ src/main/java/com/gildedgames/aether/mixin/SoundHelperAccessor.java
@@ -4,10 +4,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.client.sound.SoundHelper;
+import paulscode.sound.SoundSystem;
 
 @Mixin(SoundHelper.class)
 public interface SoundHelperAccessor {
 	
 	@Accessor("soundUID")
 	int getSoundUID();
+	@Accessor("soundSystem")
+	public static SoundSystem getSoundSystem() {
+		throw new AssertionError();
+	};
+	
+	@Accessor("musicCountdown")
+	void setMusicCountdown(int i);
 }
